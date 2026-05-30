53009ed2253acc191170157944e406829960490f
sgopi12 <sgopi5080@gmail.com>
2026-05-30 18:11:27 +0530

    practices

--- 05-datatype.sh
+++ 05-datatype.sh
@@ -10,3 +10,6 @@ echo "Sum is:$SUM"
 #Array
 MOVIES=("RRR" "VARANASI" "Cooli")  #index is always start from 0
 echo "Movies are: ${MOVIES[@]}"
+echo "first movie is ${MOVIE[0]}"
+echo "second movie is ${MOVIE[1]}"
+echo "third movie is ${MOVIE[2]}"
