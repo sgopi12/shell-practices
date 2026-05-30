ef79c54882659c2d96653937fc8edf2712751b4b
sgopi12 <sgopi5080@gmail.com>
2026-05-30 18:06:15 +0530

    practices

--- 05-datatype.sh
+++ 05-datatype.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 NUM1=10
-NUM2=30
+NUM2=Linux
 
 SUM=$((NUM1+NUM2))
 
 echo "Sum is:$SUM"
+
+#Array
+MOVIES=("RRR" "VARANASI" "Cooli")  #index is always start from 0
+echo "Movies are: ${MOVIES[@]}"
