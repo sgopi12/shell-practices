bd2dc98e18b4edd6de9ff9bfe8e245d99687cfeb
sgopi12 <sgopi5080@gmail.com>
2026-05-30 19:24:51 +0530

    intalling

--- 07-install.sh
+++ 07-install.sh
@@ -3,9 +3,19 @@
 USERID=$(id -u)
 
 # check root access or not
-if [$USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
   echo "please run this script with root access"
    exit 1
 fi
 
-echo "iam continuing..."
+# echo "iam continuing..."
+
+echo "Installing MySQL"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing MySQL is ... FAILED"
+    exit 1  
+else
+    echo "Installing MYSQL is ... SUCCESS"
+fi
