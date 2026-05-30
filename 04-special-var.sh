594d2001754953f4e84c71c46006cf9505ea9bfc
sgopi12 <sgopi5080@gmail.com>
2026-05-30 17:17:06 +0530

    commit

--- 04-special-var.sh
+++ 04-special-var.sh
@@ -8,3 +8,6 @@ echo "Script name: $0"
 echo "who is running this: $USER"
 echo "Which diectory: $PWD"
 echo "Home directory: $HOME" 
+echo "PID of the currect script: $$"
+sleep  5 &
+echo "PID of the background command running just now: $1"
