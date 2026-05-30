905f8afdbdcb223b6342ba26a1f2da055ef218c9
sgopi12 <sgopi5080@gmail.com>
2026-05-30 17:03:47 +0530

    commit

---
+++ 04-special-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##### special vars #####
+echo "All variable passed to script: $@"
+echo "Number of variables passed: $#"
+echo "First variable: $1"
+echo "Script name: $0"
+echo "who is running this: $USER"
+echo "Which diectory: $PWD"
+echo "Home directory: $HOME" 
