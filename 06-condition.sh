0f57915258b482b97a5900c7cb110f94ea663deb
sgopi12 <sgopi5080@gmail.com>
2026-05-30 18:39:55 +0530

    practices

--- 06-condition.sh
+++ 06-condition.sh
@@ -12,7 +12,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
       echo "given number $NUMBER is greater then or equal to 20"
-elif [$NUMBER eq 20]
+elif [$NUMBER eq 20]; then
       echo "given number $NUMBER is equal to 20"
 else
     echo "given number $NUMBER is less then 20"
