4e93db81bf4cbad2106921f693749b896095c8f2
sgopi12 <sgopi5080@gmail.com>
2026-05-30 13:07:21 +0530

    conversion

---
+++ 02-conversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+PERSON1=Russia
+PERSON2=Ukraine
+
+echo "$PERSON1: Hey Ukraine we are going to blash you!"
+echo "$PERSON2: Yes, please. you can welcome"
+echo "$PERSON1: Hey $PERSON2 I am serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
+
+
